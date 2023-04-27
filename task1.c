471136f171654e9d2527d7264501a986d4e175f0
Bide1101 <olabiyibisolad@gmail.com>
2023-04-27 11:57:43 -0700

    Betty style

--- task1.c
+++ task1.c
@@ -1,15 +1,19 @@
 #include "header.h"
 
+/**
+* main - Entry point of the program.
+* @argc: argument count
+* @argv: argument vector (an array of strings)
+* Return: 0 if SUCCESS and -1 if FAILURE
+*/
+
 int main(int argc  __attribute__((unused)), char **argv)
 {
-	char *lineP = NULL;
-	char *token, *delimiter = " \t\n";
+	char *lineP = NULL, *token, *delimiter = " \t\n";
 	size_t size = 0;
 	ssize_t rline = 0;
 	pid_t process;
-	char *args[1024];
-	char *command = argv[0];
-	extern char **environ;
+	char *args[1024], *command = argv[0];
 	int child, status = 1, i = 0;
 
 	while (1)
@@ -17,21 +21,16 @@ int main(int argc  __attribute__((unused)), char **argv)
 		write(STDOUT_FILENO, "$ ", 2);
 		rline = getline(&lineP, &size, stdin);
 		if (rline == -1 || (rline == 0 && lineP[0] == '\0'))
-		{
-			free(lineP);
+		{	free(lineP);
 			exit(0);
 		}
 		if (lineP == NULL || lineP[0] == '\0' || lineP[0] == ' ' || lineP[0] == '\n')
 			continue;
 		rline = _strlen(lineP);
-		if (rline > 0 && lineP[rline -1] == '\n')
+		if (rline > 0 && lineP[rline - 1] == '\n')
 			lineP[rline - 1] = '\0';
 		process = fork();
-		if (process == -1)
-		{
-			perror("Couldn't create child process");
-		}
-		else if (process == 0)
+		if (process == 0)
 		{
 			token = strtok(lineP, delimiter);
 			while (token != NULL)
@@ -41,8 +40,7 @@ int main(int argc  __attribute__((unused)), char **argv)
 			}
 			args[i] = NULL;
 			if (execve(args[0], args, environ) == -1)
-			{
-				perror(command);
+			{	perror(command);
 				exit(EXIT_FAILURE);
 			}
 		}
