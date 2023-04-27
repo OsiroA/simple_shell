1b4498053b4638938329be892aded0cd283023f3
OsiroA <osiroasunde@gmail.com>
2023-04-27 05:59:03 +0100

    betty

--- main.c
+++ main.c
@@ -1,29 +1,41 @@
 #include "shell.h"
 
 /**
-* handle_condition - handles special conditions of user input
-* @lineptr: input string from user
-* Return: 0 on success, 1 on exit command, -1 on error
+* main - Entry point of the program.
+* @argc: argument count
+* @argv: argument vector (an array of strings)
+* Return: 0 if SUCCESS and -1 if FAILURE
 */
 
-int handle_condition(char *lineptr)
+int main(int argc, char **argv)
 {
-	char **argv = NULL;
+	char *lineptr = NULL;
 	char *delim = " \t\n";
+	size_t size = 0;
 	ssize_t readline = 0;
+	(void)argc;
 
-	if (lineptr == NULL || *lineptr == '\0') /* handle empty input */
-		return (0);
-	readline = _strlen(lineptr);
-	if (readline > 0 && lineptr[readline - 1] == '\n')
-		lineptr[readline - 1] = '\0';
-	argv = split_string(lineptr, delim, readline);
-	if (argv == NULL)
-		return (-1);
-	if (pipeCheck(argv))
-		handle_pipes(argv);
-	else
-	{
+	while (1)	/* the loop to keep the shell prompt running*/
+	{	write(STDOUT_FILENO, "$ ", 2);
+		readline = my_getline(&lineptr, &size, stdin);
+		if (readline == -1)	 /* error handling for ctrl D*/
+		{	perror("exit");
+			free(lineptr);
+			return (-1);
+		}
+		readline = _strlen(lineptr);
+		if (readline > 0 && lineptr[readline - 1] == '\n')
+			lineptr[readline - 1] = '\0';
+		argv = split_string(lineptr, delim, readline);
+		free(lineptr);
+		lineptr = NULL;
+		size = 0;
+		if (argv == NULL)
+			return (-1);
+		if (pipeCheck(argv))
+			handle_pipes(argv);
+		else
+		{
 		if (_strcmp(argv[0], "cd") == 0)
 			cd_cmd(argv);
 		else if (_strcmp(argv[0], "help") == 0)
@@ -34,6 +46,7 @@ int handle_condition(char *lineptr)
 			_exec(argv);
 	}
 	free(lineptr);
+	}
 	free(argv);
 	return (0);
 }
